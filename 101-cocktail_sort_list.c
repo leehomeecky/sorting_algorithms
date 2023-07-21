72ffc4689c490d9ffc8566e46404718da03c322c
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-21 20:34:23 +0100

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -14,24 +14,21 @@ void swap_nodes(listint_t **list, listint_t *node1,
 {
 	listint_t *first = node1;
 	listint_t *second = node2;
-	 listint_t *temp;
 
-    /* Update next and prev pointers of adjacent nodes */
-    if (first->prev != NULL)
-        first->prev->next = second;
-    else
-        *list = second;
+	/*Update next and prev pointers of adjacent nodes */
+	if (first->prev != NULL)
+		first->prev->next = second;
+	else
+		*list = second;
 
-    if (second->next != NULL)
-        second->next->prev = first;
+	second->prev = first->prev;
+	first->prev = second;
+	first->next = second->next;
 
-    first->prev = second->prev;
-    second->prev = first;
+	if (second->next != NULL)
+		second->next->prev = first;
 
-    /* Swap the next pointers */
-    temp = first->next;
-    first->next = second->next;
-    second->next = temp;
+	second->next = first;
 	/*innernode = second;*/
 }
 /**
