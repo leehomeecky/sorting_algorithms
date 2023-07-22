032695bdb55cea6a544a79c2b9a835640df690f7
leehomeecky <meecky4christ@gmail.com>
2023-07-22 06:36:36 +0100

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,64 @@
+#include "sort.h"
+
+void insert(listint_t **list, listint_t *val);
+
+/**
+ * insert - a function to insert a value into a lincked list
+ * 
+ * @list: pointer to the list to insert into
+ * @val: value to insert
+ */
+
+void insert(listint_t **list, listint_t *val)
+{
+	listint_t *temp = *list;
+	
+	while (temp)
+	{
+		if (temp->n > val->n)
+		{
+			val->prev = temp->prev;
+			val->next = temp; 
+			if (temp->prev != NULL)
+				temp->prev->next = val;
+			else
+				*list = val;
+			temp->prev = val;
+			return;
+		}
+		else if (temp->next == NULL)
+		{
+			val->prev = temp;
+			val->next = temp->next;
+			temp->next = val;
+			return;
+		}
+		temp = temp->next;
+	}
+
+}
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ *
+ * @list: pointer to list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *val, *temp = *list;
+
+	while (temp->next)
+	{
+		if (temp->next->n < temp->n)
+		{
+			val = temp->next;
+			val->next->prev = val->prev;
+			val->prev->next = val->next;
+			insert(list, val);
+			print_list(*list);
+		}
+		else
+		temp = temp->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,7 +1,22 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,12 +27,30 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
+void swap_func(int *array, int idx1, int idx2);
+void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_sel(int *array, int idx1, int idx2);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+listint_t *swap_nodes(listint_t **list, listint_t *node1,
+		listint_t *node2, int pos);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+int getMax(int a[], int n);
+void countingSort(int a[], int n, int place);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quickSorthelper(int *arr, int start, int end, size_t size);
+void quick_sort_hoare(int *arr, int size);
 #endif
